28b1cc3efec9212a921d91327e26be83a919eda1
renanwilson <renan.alves.ra16@gmail.com>
2023-08-03 14:04:38 -0300

    fix: add dayjs

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -1,4 +1,6 @@
+import dayjs from 'dayjs';
+
 export const getDate = (dateString: string) => {
   const date = new Date(dateString);
-  return date.toLocaleDateString();
+  return dayjs(date).format('DD/MM/YYYY');
 };
